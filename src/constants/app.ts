21cee24f31fd84a8ddd1da4a83dbe029174461db
kanakkholwal <kanakkholwal@gmail.com>
2026-02-18 16:57:44 +0530

    feat: add Google Analytics and AdSense configuration to enhance tracking and monetization

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,8 @@
+import {
+    PUBLIC_ADSENSE_PUBLISHER_ID,
+    PUBLIC_GOOGLE_ANALYTICS_ID
+} from '$env/static/public';
+
 export const config = {
     appName: 'Nexo PDF',
     appVersion: '0.1.0',
@@ -5,5 +10,6 @@ export const config = {
           locally via WebAssembly without them ever touching a server.`,
     supportEmail: "support@nexonauts.com",
     github: "https://github.com/kanakkholwal/nexo-pdf",
-    adsensePublisherId: "ca-pub-6988693445063744",
+    adsensePublisherId: PUBLIC_ADSENSE_PUBLISHER_ID || "",
+    googleAnalyticsId: PUBLIC_GOOGLE_ANALYTICS_ID || "",
 }
