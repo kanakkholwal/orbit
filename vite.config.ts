b44f64ce747c0aa85825326783d89d00673debaa
kanakkholwal <kanakkholwal@gmail.com>
2026-02-25 19:52:41 +0530

    fix: comment out permissions in release workflow and update package.json for Android dev script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 
+
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
@@ -15,5 +16,13 @@ export default defineConfig({
 		port: 3000,
 		open: true,
 		strictPort: true,
-	}
+		host:"0.0.0.0",
+		watch: {
+			// tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**'],
+		},
+
+	},
+	// Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+	envPrefix: ['VITE_', 'TAURI_ENV_*']
 });
