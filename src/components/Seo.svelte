fec6d751321d659f72688d80c9f4cab5cee41314
kanakkholwal <kanakkholwal@gmail.com>
2026-02-28 23:01:45 +0530

    fix: correct meta property names and improve formatting in SEO component

--- src/components/Seo.svelte
+++ src/components/Seo.svelte
@@ -5,7 +5,7 @@
 
   export let title = config.appName;
   export let description = config.appDescription;
-  export let keywords:string[] = []
+  export let keywords: string[] = [];
   export let image = config.appDomain + "/og.png";
   export let isBase = false;
 
@@ -17,26 +17,25 @@
 <svelte:head>
   <title>{isBase ? title : `${title} | ${config.appName}`}</title>
   <meta name="description" content={description} />
-  <meta property="og_site_name" content={config.appDomain} />
+  <meta property="og:site_name" content={config.appDomain} />
   <meta property="og:url" content={canonicalUrl} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="keywords" content={keywords.join(",")}/>
+  <meta property="keywords" content={keywords.join(",")} />
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content={config.appDomain} />
   <meta property="twitter:url" content={canonicalUrl} />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
 
-  {@html `  <script type="application/ld+json">{
-   "@context": "https://schema.org",
-   "@type": "Website",
-   "name": "${title} | ${config.appName}",
-   "url": "${canonicalUrl}"",
-   ${image ? `"logo": "${image}",` : ''}
-  }</script>`}
-
+  {@html `<script type="application/ld+json">${JSON.stringify({
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: `${title} | ${config.appName}`,
+    url: canonicalUrl,
+    ...(image ? { logo: image } : {}),
+  })}</script>`}
   {#if image}
     <meta name="twitter:image" content={image} />
     <meta property="og:image" content={image} />
