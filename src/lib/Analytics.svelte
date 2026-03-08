8a72de176abfd85c6280e8b28d2340989772cfd3
kanakkholwal <kanakkholwal@gmail.com>
2026-03-08 17:10:07 +0530

    feat: refactor Google Analytics integration to improve script loading and data layer initialization

--- src/lib/Analytics.svelte
+++ src/lib/Analytics.svelte
@@ -1,36 +1,29 @@
 <script lang="ts">
   import { browser } from '$app/environment';
-  import { page } from '$app/stores';
   import { config } from '$constants/app';
   import { onMount } from 'svelte';
-
-  const GA_ID = config.googleAnalyticsId;
+  
+  let { googleAnalyticsId }  = config
 
   onMount(() => {
-    if (!browser || !GA_ID) return;
+    if (!browser || !googleAnalyticsId) return;
 
-    // Inject the gtag script tag
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-    document.head.appendChild(script);
-
-    // Initialise the data layer
     window.dataLayer = window.dataLayer || [];
-    window.gtag = function (...args: unknown[]) {
-      window.dataLayer.push(args);
-    };
-    window.gtag('js', new Date());
-    window.gtag('config', GA_ID, { send_page_view: false });
-  });
+    function gtag() {
+      // @ts-ignore
+      window.dataLayer.push(arguments);
+    }
+    // @ts-ignore
+    gtag("js", new Date());
 
-  // Track page navigation as virtual page views
-  $effect(() => {
-    if (!browser || !GA_ID || typeof window.gtag !== 'function') return;
-    const url = $page.url.pathname + $page.url.search;
-    window.gtag('event', 'page_view', {
-      page_path: url,
-      page_title: document.title,
-    });
+    // @ts-ignore
+    gtag("config", googleAnalyticsId);
   });
 </script>
+
+<svelte:head>
+  {#if googleAnalyticsId && browser}
+    <script async src="https://www.googletagmanager.com/gtag/js?id={googleAnalyticsId}"></script>
+  {/if}
+</svelte:head>
+
